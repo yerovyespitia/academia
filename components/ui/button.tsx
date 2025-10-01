965e4bbf34bf10c39a71f9b4e9f9e4cea6dd7268
Yerovy Espitia <luyeesgar@gmail.com>
2025-09-30 22:42:34 -0500

    feat: add shadcn components and update variants

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,11 +19,13 @@ const buttonVariants = cva(
         ghost:
           "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50",
         link: "text-primary underline-offset-4 hover:underline",
+        brand: "bg-primary text-primary-foreground hover:bg-primary/90 rounded-full transition-transform duratino-150 active:scale-90 cursor-pointer",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
+        brand: "h-10 px-6 has-[>svg]:px-4",
         icon: "size-9",
       },
     },
