752c1cc9b3ab3a712e457941168c8ef5f489103d
Yerovy Espitia <luyeesgar@gmail.com>
2025-10-03 09:44:01 -0500

    feat: add SubNavbar

---
+++ components/sub-navbar/index.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link'
+
+export default function SubNavbar() {
+  return (
+    <nav className='border-b border-border'>
+      <header className='max-w-7xl mx-auto flex justify-center items-center p-4'>
+        <div className='flex items-center justify-center gap-4 flex-wrap'>
+          <Link
+            className='font-medium text-muted-foreground hover:text-foreground'
+            href='/'
+          >
+            Documentos
+          </Link>
+          <Link
+            className='font-medium text-muted-foreground hover:text-foreground'
+            href='/'
+          >
+            Quizzes
+          </Link>
+          <Link
+            className='font-medium text-muted-foreground hover:text-foreground'
+            href='/'
+          >
+            Glosarios
+          </Link>
+          <Link
+            className='font-medium text-muted-foreground hover:text-foreground'
+            href='/'
+          >
+            Mapas conceptuales
+          </Link>
+          <Link
+            className='font-medium text-muted-foreground hover:text-foreground'
+            href='/'
+          >
+            Etiquetas
+          </Link>
+          <Link
+            className='font-medium text-muted-foreground hover:text-foreground'
+            href='/'
+          >
+            Historial académico
+          </Link>
+        </div>
+      </header>
+    </nav>
+  )
+}
