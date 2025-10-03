9ca4065145dcc2923d4bc9f015673dd65e93639b
Yerovy Espitia <luyeesgar@gmail.com>
2025-10-03 11:46:47 -0500

    feat(layout): add UploadNotes

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import GradesPanel from '@/components/grades-panel'
+import StudyTools from '@/components/study-tools'
 
 export default function Home() {
   return (
     <main className='max-w-7xl mx-auto p-4'>
       <GradesPanel />
+      <StudyTools />
     </main>
   )
 }
