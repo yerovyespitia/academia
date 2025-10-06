05d9fa67abcb8402dfd5b28ac956e06a779cc8de
Yerovy Espitia <luyeesgar@gmail.com>
2025-10-05 19:35:47 -0500

    style: update shadcn components

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -9,23 +9,23 @@ const buttonVariants = cva(
   {
     variants: {
       variant: {
-        default: "bg-primary text-primary-foreground hover:bg-primary/90 cursor-pointer",
+        default: "bg-primary text-primary-foreground hover:bg-primary/90 transition-transform duration-150 active:scale-90 cursor-pointer",
         destructive:
-          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 cursor-pointer",
+          "bg-destructive text-white hover:bg-destructive/90 focus-visible:ring-destructive/20 dark:focus-visible:ring-destructive/40 dark:bg-destructive/60 transition-transform duration-150 active:scale-90 cursor-pointer",
         outline:
-          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 cursor-pointer",
+          "border bg-background shadow-xs hover:bg-accent hover:text-accent-foreground dark:bg-input/30 dark:border-input dark:hover:bg-input/50 transition-transform duration-150 active:scale-90 cursor-pointer",
         secondary:
-          "bg-secondary text-secondary-foreground hover:bg-secondary/80 cursor-pointer",
+          "bg-secondary text-secondary-foreground hover:bg-secondary/80 transition-transform duration-150 active:scale-90 cursor-pointer",
         ghost:
-          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 cursor-pointer",
-        link: "text-primary underline-offset-4 hover:underline cursor-pointer",
+          "hover:bg-accent hover:text-accent-foreground dark:hover:bg-accent/50 transition-transform duration-150 active:scale-90 cursor-pointer",
+        link: "text-primary underline-offset-4 hover:underline transition-transform duration-150 active:scale-90 cursor-pointer",
         brand: "bg-primary text-primary-foreground hover:bg-primary/90 rounded-full transition-transform duration-150 active:scale-90 cursor-pointer",
       },
       size: {
         default: "h-9 px-4 py-2 has-[>svg]:px-3",
         sm: "h-8 rounded-md gap-1.5 px-3 has-[>svg]:px-2.5",
         lg: "h-10 rounded-md px-6 has-[>svg]:px-4",
-        brand: "h-10 px-6 has-[>svg]:px-4",
+        brand: "h-10 px-6 has-[>svg]:px-4 rounded-full",
         icon: "size-9",
       },
     },
