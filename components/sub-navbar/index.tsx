b6b465f24970c26469f076d7d51e5dc9dd39be40
Yerovy Espitia <luyeesgar@gmail.com>
2025-10-05 19:37:10 -0500

    refactor: use dynamic links and highlight active route

--- components/sub-navbar/index.tsx
+++ components/sub-navbar/index.tsx
@@ -1,46 +1,54 @@
-import Link from 'next/link'
+'use client'
 
+import Link from 'next/link'
+import { usePathname } from 'next/navigation'
+
+const links = [
+  {
+    label: 'Documentos',
+    href: '/documents',
+  },
+  {
+    label: 'Quizzes',
+    href: '/',
+  },
+  {
+    label: 'Glosarios',
+    href: '/',
+  },
+  {
+    label: 'Mapas conceptuales',
+    href: '/',
+  },
+  {
+    label: 'Etiquetas',
+    href: '/',
+  },
+  {
+    label: 'Historial académico',
+    href: '/',
+  },
+]
 export default function SubNavbar() {
+  const pathname = usePathname()
+
   return (
     <nav className='border-b border-muted-foreground/30'>
       <header className='max-w-7xl mx-auto flex justify-center items-center p-4'>
         <div className='flex items-center justify-center gap-4 flex-wrap'>
-          <Link
-            className='font-medium text-muted-foreground hover:text-foreground'
-            href='/'
-          >
-            Documentos
-          </Link>
-          <Link
-            className='font-medium text-muted-foreground hover:text-foreground'
-            href='/'
-          >
-            Quizzes
-          </Link>
-          <Link
-            className='font-medium text-muted-foreground hover:text-foreground'
-            href='/'
-          >
-            Glosarios
-          </Link>
-          <Link
-            className='font-medium text-muted-foreground hover:text-foreground'
-            href='/'
-          >
-            Mapas conceptuales
-          </Link>
-          <Link
-            className='font-medium text-muted-foreground hover:text-foreground'
-            href='/'
-          >
-            Etiquetas
-          </Link>
-          <Link
-            className='font-medium text-muted-foreground hover:text-foreground'
-            href='/'
-          >
-            Historial académico
-          </Link>
+          {links.map((link) => (
+            <Link
+              key={link.label}
+              className={`font-medium hover:text-foreground ${
+                pathname === link.href
+                  ? 'text-foreground'
+                  : 'text-muted-foreground'
+              }`}
+              href={link.href}
+            >
+              {link.label}
+            </Link>
+          ))}
         </div>
       </header>
     </nav>
