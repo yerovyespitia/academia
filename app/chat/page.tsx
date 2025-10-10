ef0cbeff5dd2dd5d99317442665691cec841afb2
Yerovy Espitia <luyeesgar@gmail.com>
2025-10-10 12:35:27 -0500

    feat: add chat route

---
+++ app/chat/page.tsx
@@ -0,0 +1,9 @@
+import ChatView from '@/components/chatview'
+
+export default function ChatPage() {
+  return (
+    <main className='max-w-7xl mx-auto p-4'>
+      <ChatView />
+    </main>
+  )
+}
